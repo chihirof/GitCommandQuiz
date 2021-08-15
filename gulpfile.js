75353037565b2dcb4f6771a0742d9d0758ba9721
chihiro <fujisawachihiro@gmail.com>
2021-08-15 14:56:02 +0900

    eslint導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,14 @@
-const fs = require("fs");
-const data = require("./data.json")
+const fs = require('fs')
+const data = require('./data.json')
 
-async function init() {
+async function init () {
   await Promise.all(data.map(async d => await createQuestion(d)))
 }
 
-async function createQuestion(d) {
-  const template = await fs.readFileSync("./templates/question.md", 'utf-8')
-  const page = template.replace("{no}", d.no).replace("{question}", d.question);
-  await fs.writeFileSync(`./questions/${d.no}.md`, page);
+async function createQuestion (d) {
+  const template = await fs.readFileSync('./templates/question.md', 'utf-8')
+  const page = template.replace('{no}', d.no).replace('{question}', d.question)
+  await fs.writeFileSync(`./questions/${d.no}.md`, page)
 }
 
-exports.init = init;
+exports.init = init
